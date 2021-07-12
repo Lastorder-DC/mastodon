c75e85439e026fee78de6795c7b800ad30cccbf1
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2021-07-12 18:07:39 +0900

    Make default privacy setting to unlisted

--- app/models/user.rb
+++ app/models/user.rb
@@ -310,7 +310,7 @@ class User < ApplicationRecord
   end
 
   def setting_default_privacy
-    settings.default_privacy || (account.locked? ? 'private' : 'public')
+    settings.default_privacy || (account.locked? ? 'private' : 'unlisted')
   end
 
   def allows_report_emails?
