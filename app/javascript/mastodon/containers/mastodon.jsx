57bfbd466e7ad9133eca2cea122870b9e8529dba
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2025-11-28 01:05:28 +0000

    WIP MA

--- app/javascript/mastodon/containers/mastodon.jsx
+++ app/javascript/mastodon/containers/mastodon.jsx
@@ -13,11 +13,16 @@ import ErrorBoundary from 'mastodon/components/error_boundary';
 import { Router } from 'mastodon/components/router';
 import UI from 'mastodon/features/ui';
 import { IdentityContext, createIdentityContext } from 'mastodon/identity_context';
-import { initialState, title as siteTitle } from 'mastodon/initial_state';
+import initialState, { title as siteTitle, getAccessToken } from 'mastodon/initial_state';
 import { IntlProvider } from 'mastodon/locales';
 import { store } from 'mastodon/store';
 import { isProduction } from 'mastodon/utils/environment';
 import { BodyScrollLock } from 'mastodon/features/ui/components/body_scroll_lock';
+import { setActiveAccountToken } from 'mastodon/api';
+import {
+  hydrateStore as hydrateMultiAccountStore,
+  attachPersistence as attachMultiAccountPersistence,
+} from 'mastodon/utils/multi_account_storage';
 
 import { ScrollContext } from './scroll_container/scroll_context';
 
@@ -30,10 +35,45 @@ if (initialState.meta.me) {
   store.dispatch(fetchCustomEmojis());
 }
 
+void hydrateMultiAccountStore(store).then(() => {
+  attachMultiAccountPersistence(store);
+});
+
+const bootstrapToken = getAccessToken();
+if (bootstrapToken) {
+  setActiveAccountToken(bootstrapToken);
+}
+
 export default class Mastodon extends PureComponent {
   identity = createIdentityContext(initialState);
+  unsubscribeStore = null;
+
+  updateIdentityFromStore = () => {
+    const state = store.getState();
+
+    const accountId = state.getIn(['meta', 'me']);
+    const disabledAccountId = state.getIn(['meta', 'disabled_account_id']);
+    const permissions = state.getIn(['role', 'permissions']) ?? 0;
+    const signedIn = !!accountId;
+
+    if (
+      this.identity.accountId !== accountId ||
+      this.identity.disabledAccountId !== disabledAccountId ||
+      this.identity.permissions !== permissions ||
+      this.identity.signedIn !== signedIn
+    ) {
+      this.identity = {
+        signedIn,
+        accountId,
+        disabledAccountId,
+        permissions,
+      };
+      this.forceUpdate();
+    }
+  };
 
   componentDidMount() {
+    this.unsubscribeStore = store.subscribe(this.updateIdentityFromStore);
     if (this.identity.signedIn) {
       this.disconnect = store.dispatch(connectUserStream());
     }
@@ -43,6 +83,10 @@ export default class Mastodon extends PureComponent {
     if (this.disconnect) {
       this.disconnect();
       this.disconnect = null;
+    }
+    if (this.unsubscribeStore) {
+      this.unsubscribeStore();
+      this.unsubscribeStore = null;
     }
   }
 
